0db5f9e92de636063a3c34d8298ca02a3141cea1
Nicholas E. Rabenau <nerab@gmx.at>
2018-10-09 20:57:50 +0200

    Upgrade to Ruby 2.5.1

--- files/etc/profile.d/gem-user_dir-path.sh
+++ files/etc/profile.d/gem-user_dir-path.sh
@@ -1,4 +1,4 @@
 # http://guides.rubygems.org/faqs/#user-install
 if which ruby >/dev/null && which gem >/dev/null; then
-  PATH="$(ruby -rubygems -e 'puts Gem.user_dir')/bin:$PATH"
+  PATH="$(ruby -e 'puts Gem.user_dir')/bin:$PATH"
 fi
