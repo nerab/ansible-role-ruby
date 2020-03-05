e2026b6422ca2f23ced4daac8fcff67ce109c990
Nicholas E. Rabenau <nerab@gmx.at>
2020-03-05 21:36:44 +0100

    Decrease default log level

--- tests/lib/vagrant/logger.rb
+++ tests/lib/vagrant/logger.rb
@@ -7,7 +7,7 @@ module Vagrant
     end
 
     def logger
-      @logger ||= ::Logger.new(STDERR).tap { |logger| logger.level = ::Logger::DEBUG }
+      @logger ||= ::Logger.new(STDERR).tap { |logger| logger.level = ::Logger::ERROR }
     end
   end
 end
